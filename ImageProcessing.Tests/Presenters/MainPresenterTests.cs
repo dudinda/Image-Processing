dcf6ccb12b7b982100c2180b591c0844ce1b8f63
Даниил Дудин <dudin@cmsplanet.ru>
2020-01-09 23:57:40 +0300

    Added test project

---
+++ ImageProcessing.Tests/Presenters/MainPresenterTests.cs
@@ -0,0 +1,36 @@
+﻿using ImageProcessing.Factory.Base;
+using ImageProcessing.Presentation.AppController;
+using ImageProcessing.Presentation.Presenters;
+using ImageProcessing.Presentation.Views.Main;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace ImageProcessing.Tests.Presenters
+{
+    [TestFixture]
+    public class MainPresenterTests
+    {
+        private IAppController _controller;
+        private MainPresenter _presenter;
+        private IMainView _view;
+        private IBaseFactory _baseFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller  = Substitute.For<IAppController>();
+            _baseFactory = Substitute.For<IBaseFactory>();
+            _view        = Substitute.For<IMainView>();
+
+            _presenter = new MainPresenter(_controller, _view, _baseFactory);
+            _presenter.Run();
+        }
+
+        [TestCase("BoxBlur3x3", "GaussianBlur3x3")]
+        public async void ApplyConvolutionFilter(string filterName)
+        {
+            
+        } 
+    }
+}
---
+++ ImageProcessing.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageProcessing.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageProcessing.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ec2495b-c648-468b-b0c3-2cd6375b3aeb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
