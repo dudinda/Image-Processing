d72a9b34cf9527fa15ae1eb7efc2f92c662f169f
Даниил Дудин <dudin@cmsplanet.ru>
2020-01-12 14:56:03 +0300

    Refactoring

---
+++ Source/ImageProcessing.Services/ConvolutionFilter/IConvolutionFilterService.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+using ImageProcessing.ConvulationFilters;
+
+namespace ImageProcessing.Services.ConvolutionFilter
+{
+    public interface IConvolutionFilterService
+    {
+        Bitmap Convolution(Bitmap source, AbstractConvolutionFilter filter);
+    }
+}
---
+++ Source/ImageProcessing.Services/RGBFilter/IRGBFilterService.cs
@@ -0,0 +1,11 @@
+﻿using ImageProcessing.RGBFilters.Interface;
+
+using System.Drawing;
+
+namespace ImageProcessing.Services.RGBFilter
+{
+    public interface IRGBFilterService
+    {
+        Bitmap Filter(Bitmap source, IRGBFilter filter);
+    }
+}
