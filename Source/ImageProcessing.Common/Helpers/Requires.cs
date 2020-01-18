786619e4dd1ddb6f1bbde3a0abfe6ead05606d9e
Даниил Дудин <dudin0da@gmail.com>
2020-01-18 16:11:52 +0300

    Add requires helper

---
+++ Source/ImageProcessing.Common/Helpers/Requires.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessing.Common.Helpers
+{
+    public static class Requires
+    {
+        public static T IsNotNull<T>(T instance, string paramName) where T : class
+        {
+            // Use ReferenceEquals in case T overrides equals.
+            if (object.ReferenceEquals(null, instance)) {
+                throw new ArgumentNullException(paramName);
+            }
+
+            return instance;
+        }
+        
+        public static void IsTrue(Func<bool> isTrue, string errorMessage)
+        {
+           IsNotNull(isTrue, nameof(isTrue));
+
+            if(!isTrue())
+            {
+                throw new ArgumentException(errorMessage);
+            }
+        }
+    }
+}
