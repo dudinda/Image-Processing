e03df33760e503285c941cfdde8b6264ad382d95
Даниил Дудин <dudin0da@gmail.com>
2020-01-22 00:48:18 +0300

    Add control for distribution operations on grayscales

---
+++ Source/ImageProcessing.UI/Control/AppendProcessedBitmapToolStripButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageProcessing.UI.Control
+{
+    public class AppendProcessedBitmapToolStripButton : ToolStripButton
+    {
+        public Queue<Bitmap> Queue { get; } = new Queue<Bitmap>();
+
+        public bool IsQueued(Bitmap bitmap)
+            => Queue.Any(bmp => bmp.Tag.Equals(bitmap.Tag));
+
+        public bool Add(Bitmap bitmap)
+        {
+            if(!IsQueued(bitmap))
+            {
+                Queue.Enqueue(bitmap);
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            Queue.Clear();
+        }
+        
+
+    }
+}
