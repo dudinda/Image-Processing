1d72870b82739229abe999497c7694ef4dcda584
Daniil Dudin <dudin0da@gmail.com>
2020-01-29 00:56:24 +0300

    Add core pipeline interface

---
+++ Source/ImageProcessing.Core/Pipeline/IPipeline.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ImageProcessing.Core.Pipeline
+{
+    public interface IAwaitablePipeline<TOutput>
+    {
+        Task<TOutput> Execute(object input);
+    }
+}
---
+++ Source/ImageProcessing.Services/Pipeline/PipelineService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessing.Core.Pipeline;
+
+namespace ImageProcessing.Services.Pipeline
+{
+    public class PipelineService<TOutput> : IAwaitablePipeline<TOutput>
+    {
+        public Task<TOutput> Execute(object input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
