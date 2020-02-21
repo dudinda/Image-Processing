96cd625ebc482abc085377022e4fe15dea94abf7
Daniil Dudin <dudin0da@gmail.com>
2020-02-21 17:03:39 +0300

    code formatting

--- Source/ImageProcessing.Core/Presenter/Abstract/BasePresenter.cs
+++ Source/ImageProcessing.Core/Presenter/Abstract/BasePresenter.cs
@@ -6,38 +6,38 @@ using ImageProcessing.Core.View;
 namespace ImageProcessing.Core.Presenter.Abstract
 {
 	public abstract class BasePresenter<TView> : IPresenter
-        where TView : class, IView
-    {
-        protected TView View { get; }
-        protected IAppController Controller { get; }
+		where TView : class, IView
+	{
+		protected TView View { get; }
+		protected IAppController Controller { get; }
 
-        protected BasePresenter(IAppController controller, TView view)
-        {
-            Controller = controller ?? throw new ArgumentNullException(nameof(controller));
+		protected BasePresenter(IAppController controller, TView view)
+		{
+			Controller = controller ?? throw new ArgumentNullException(nameof(controller));
 			View = view ?? throw new ArgumentNullException(nameof(view));
-        }
+		}
 
-        public virtual void Run() => View.Show();
-    }
+		public virtual void Run() => View.Show();
+	}
 
-    public abstract class BasePresenter<TView, TViewModel> : IPresenter<TViewModel>
-        where TView : class, IView
+	public abstract class BasePresenter<TView, TViewModel> : IPresenter<TViewModel>
+		where TView : class, IView
 		where TViewModel : class
-    {
-        protected TView View { get; }
+	{
+		protected TView View { get; }
 		protected IAppController Controller { get; }
 		protected TViewModel ViewModel { get; private set; }
-      
-        protected BasePresenter(IAppController controller, TView view)
-        {
-            Controller = controller ?? throw new ArgumentNullException(nameof(controller));
-			View = view ?? throw new ArgumentNullException(nameof(view));
-        }
 
-        public virtual void Run(TViewModel argument)
-        {
-            ViewModel = argument ?? throw new ArgumentNullException(nameof(argument));
-            View.Show();
-        }
-    }
+		protected BasePresenter(IAppController controller, TView view)
+		{
+			Controller = controller ?? throw new ArgumentNullException(nameof(controller));
+			View = view ?? throw new ArgumentNullException(nameof(view));
+		}
+
+		public virtual void Run(TViewModel argument)
+		{
+			ViewModel = argument ?? throw new ArgumentNullException(nameof(argument));
+			View.Show();
+		}
+	}
 }
