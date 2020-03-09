0d2ac974b9ab6078dbb92b74e320098c78d2a150
Daniil Dudin <dudin0da@gmail.com>
2020-03-10 00:58:54 +0300

    add erosion and dilation operators

--- Source/ImageProcessing.DomainModel/Model/Morphology/Erosion/ErosionOperator.cs
+++ Source/ImageProcessing.DomainModel/Model/Morphology/Erosion/ErosionOperator.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Drawing;
-
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using ImageProcessing.Common.Extensions.BitmapExtensions;
 using ImageProcessing.Common.Helpers;
 using ImageProcessing.Common.Utility.BitMatrix.Implementation;
 using ImageProcessing.Core.Model.Morphology.UnaryOperator;
@@ -17,7 +20,73 @@ namespace ImageProcessing.DomainModel.Model.Morphology
             Requires.IsNotNull(bitmap, nameof(bitmap));
             Requires.IsNotNull(kernel, nameof(kernel));
 
-            throw new System.NotImplementedException();
+            var destination = new Bitmap(bitmap);
+
+            var source = bitmap.AdjustBorder(kernel.ColumnCount / 2, Color.Black);
+
+            var sourceBitmapData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height),
+                                                 ImageLockMode.ReadOnly,
+                                                 source.PixelFormat);
+
+            var destinationBitmapData = destination.LockBits(new Rectangle(0, 0, source.Width, source.Height),
+                                                             ImageLockMode.WriteOnly,
+                                                             destination.PixelFormat);
+
+            var size = source.Size;
+            var ptrStep = source.GetBitsPerPixel() / 8;
+
+            unsafe
+            {
+                var sourceStartPtr      = (byte*)sourceBitmapData.Scan0.ToPointer();
+                var destinationStartPtr = (byte*)destinationBitmapData.Scan0.ToPointer();
+
+                var kernelOffset = (byte)(kernel.ColumnCount / 2);
+
+                var options = new ParallelOptions()
+                {
+                    MaxDegreeOfParallelism = Environment.ProcessorCount - 1
+                };
+
+                Parallel.For(kernelOffset, size.Height - kernelOffset, options, y =>
+                {
+                    //get the address of a new line, considering a kernel offset
+                    var sourcePtr      = sourceStartPtr + y * sourceBitmapData.Stride + kernelOffset * ptrStep;
+                    var destinationPtr = destinationStartPtr + y * destinationBitmapData.Stride + kernelOffset * ptrStep;
+
+                    //a pointer, which gets addresses of the elements in the radius of a structured element
+                    byte* elementPtr = null;
+
+                    for (int x = kernelOffset; x < size.Width - kernelOffset; ++x, sourcePtr += ptrStep, destinationPtr += ptrStep)
+                    {
+                        bool isEroded = false;
+
+                        for (int kernelRow = -kernelOffset; kernelRow <= kernelOffset; ++kernelRow)
+                        {
+                            for (int kernelColumn = -kernelOffset; kernelColumn <= kernelOffset; ++kernelColumn)
+                            {
+                                //get the address of a current element
+                                elementPtr = sourcePtr + kernelColumn * ptrStep + kernelRow * sourceBitmapData.Stride;
+
+                                if (kernel[kernelRow + kernelOffset, kernelColumn + kernelOffset] && elementPtr[0] != 255)
+                                {
+                                    destinationPtr[0] = destinationPtr[1] = destinationPtr[2] = 0;
+                                    isEroded = true;
+                                    break;
+                                }
+                            }
+
+                            if (isEroded) break;
+                        }
+                    }
+                });
+            }
+
+            source.UnlockBits(sourceBitmapData);
+            destination.UnlockBits(destinationBitmapData);
+
+            source.Dispose();
+
+            return destination;
         }
     }
 }
