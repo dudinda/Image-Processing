e1bc27668a424d7ca5494d9a80ddafa36b9180fa
Daniil Dudin <dudin0da@gmail.com>
2020-03-30 21:17:39 +0300

    update visibleto

--- Source/ImageProcessing.DomainModel/InternalsVisibleTo.cs
+++ Source/ImageProcessing.DomainModel/InternalsVisibleTo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ImageProcessing.DomainModel.Benchmark")]
 [assembly: InternalsVisibleTo("ImageProcessing.DomainModel.UnitTests")]
+[assembly: InternalsVisibleTo("ImageProcessing.ServiceLayer.Benchmark")]
 [assembly: InternalsVisibleTo("ImageProcessing.ServiceLayer.UnitTests")]
