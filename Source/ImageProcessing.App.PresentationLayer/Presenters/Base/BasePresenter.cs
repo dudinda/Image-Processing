749954d87a9d886f8eef57553ff1623a5c588211
Daniil Dudin <dudin0da@gmail.com>
2020-05-03 20:24:19 +0300

    update base presenter

--- Source/ImageProcessing.App.PresentationLayer/Presenters/Base/BasePresenter.cs
+++ Source/ImageProcessing.App.PresentationLayer/Presenters/Base/BasePresenter.cs
@@ -68,11 +68,6 @@ namespace ImageProcessing.App.PresentationLayer.Presenters.Base
                Controller.IoC.Resolve<IAwaitablePipeline>
            );
 
-        private Lazy<TView> _view
-           => new Lazy<TView>(
-               Controller.IoC.Resolve<TView>
-           );
-
         /// <inheritdoc cref="IAppController"/>
         protected IAppController Controller { get; }
 
@@ -80,11 +75,26 @@ namespace ImageProcessing.App.PresentationLayer.Presenters.Base
         protected IAwaitablePipeline Pipeline
             => _pipeline.Value;
 
+
+        private TView _view
+            = null!;
+
         /// <summary>
         /// Access point to the UI layer components.
         /// </summary>
         protected TView View
-            => _view.Value;
+        {
+            get
+            {
+                if(_view is null)
+                {
+                    _view = Controller.IoC.Resolve<TView>();
+                }
+
+                return _view;
+            }
+        }
+             
 
         /// <summary>
         /// View model of a presenter.
