576dfe37aa0e68caf903d92cb7f882828b2e6fd4
Daniil Dudin <dudin0da@gmail.com>
2020-08-01 15:48:18 +0300

    added many-to-many and one-to-many publishers

--- Source/ImageProcessing.App.DomainLayer/DomainEvent/ConvolutionArgs/ConvolutionFilterEventArgs.cs
+++ Source/ImageProcessing.App.DomainLayer/DomainEvent/ConvolutionArgs/ConvolutionFilterEventArgs.cs
@@ -1,9 +1,15 @@
+using ImageProcessing.App.CommonLayer.Enums;
 using ImageProcessing.App.DomainLayer.DomainEvent.Base;
 
 namespace ImageProcessing.App.DomainLayer.DomainEvent.ConvolutionArgs
 {
     public sealed class ConvolutionFilterEventArgs : BaseEventArgs
     {
+        public ConvolutionFilterEventArgs(object sender)
+        {
+            Sender = sender;
+        }
 
+        public object Sender { get; }
     }
 }
