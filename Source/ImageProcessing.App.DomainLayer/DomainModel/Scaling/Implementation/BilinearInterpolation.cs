100fbfca295cbe6f28689ce830eeeb1b895a56d8
Daniil Dudin <dudin0da@gmail.com>
2020-11-06 15:02:17 +0300

    updated bilinear interpolation

--- Source/ImageProcessing.App.DomainLayer/DomainModel/Scaling/Implementation/BilinearInterpolation.cs
+++ Source/ImageProcessing.App.DomainLayer/DomainModel/Scaling/Implementation/BilinearInterpolation.cs
@@ -45,12 +45,10 @@ namespace ImageProcessing.App.DomainLayer.DomainModel.Scaling.Implementation
                 var srcStartPtr = (byte*)srcData.Scan0.ToPointer();
                 var dstStartPtr = (byte*)dstData.Scan0.ToPointer();
 
-                // guarantees that on coordinate transform (x, y) -> (ax, ay)
-                // the pointer of the source image will not reach
-                // the rightmost column and the lowest row
-                var srcWidth = src.Width - 1;
-                var srcHeight = src.Height - 1;
+                var (srcWidth, srcHeight) = (src.Width, src.Height);
+                var (xBound, yBound) = (srcWidth - 2, srcHeight - 2);
 
+                //(x, y) -> (ax, ay)
                 var yCoef = srcHeight / (double)dstHeight;
                 var xCoef = srcWidth / (double)dstWidth;
 
@@ -59,54 +57,58 @@ namespace ImageProcessing.App.DomainLayer.DomainModel.Scaling.Implementation
                 Parallel.For(0, dstHeight, options, y =>
                 {
                     var newY      = y * yCoef;
-                    var newYFloor = (int)newY;
-                    var newYFrac  = newY - newYFloor;
+                    var yFlr = (int)newY;
+                    var yFrc  = newY - yFlr;
+
+                    if (yFlr > yBound) { yFlr = yBound; }
 
                     //get the address of a row
                     var dstRow = dstStartPtr + y * dstData.Stride;
 
-                    var i0 = srcStartPtr + newYFloor       * srcData.Stride;
-                    var i1 = srcStartPtr + (newYFloor + 1) * srcData.Stride;
+                    var i0 = srcStartPtr +  yFlr      * srcData.Stride;
+                    var i1 = srcStartPtr + (yFlr + 1) * srcData.Stride;
 
                     double col0, col1, point;
 
                     for (var x = 0; x < dstWidth; ++x, dstRow += ptrStep)
                     { 
                         var newX      = x * xCoef;
-                        var newXFloor = (int)newX;
-                        var newXFrac  = newX - newXFloor;
+                        var xFlr = (int)newX;
+                        var xFrc  = newX - xFlr;
 
-                        var j0 = newXFloor       * ptrStep;
-                        var j1 = (newXFloor + 1) * ptrStep;
+                        if (xFlr > xBound) { xFlr = xBound; }
+
+                        var j0 =  xFlr * ptrStep;
+                        var j1 = (xFlr + 1) * ptrStep;
 
                         var p00 = i0 + j0; var p10 = i1 + j0;                      
                         var p01 = i0 + j1; var p11 = i1 + j1;
 
-                        var invXFrac = 1 - newXFrac;
-                        var invYFrac = 1 - newYFrac;
+                        var invXFrac = 1 - xFrc;
+                        var invYFrac = 1 - yFrc;
 
-                        col0 = p00[0] * invXFrac + p10[0] * newXFrac;
-                        col1 = p01[0] * invXFrac + p11[0] * newXFrac;
+                        col0 = p00[0] * invXFrac + p10[0] * xFrc;
+                        col1 = p01[0] * invXFrac + p11[0] * xFrc;
 
-                        point = col0 * invYFrac + col1 * newYFrac;
+                        point = col0 * invYFrac + col1 * yFrc;
 
                         if (point > 255) { point = 255; } else if (point < 0) { point = 0; }
 
                         dstRow[0] = (byte)point;
 
-                        col0 = p00[1] * invXFrac + p10[1] * newXFrac;
-                        col1 = p01[1] * invXFrac + p11[1] * newXFrac;
+                        col0 = p00[1] * invXFrac + p10[1] * xFrc;
+                        col1 = p01[1] * invXFrac + p11[1] * xFrc;
 
-                        point = col0 * invYFrac + col1 * newYFrac;
+                        point = col0 * invYFrac + col1 * yFrc;
 
                         if (point > 255) { point = 255; } else if (point < 0) { point = 0; }
 
                         dstRow[1] = (byte)point;
 
-                        col0 = p00[2] * invXFrac + p10[2] * newXFrac;
-                        col1 = p01[2] * invXFrac + p11[2] * newXFrac;
+                        col0 = p00[2] * invXFrac + p10[2] * xFrc;
+                        col1 = p01[2] * invXFrac + p11[2] * xFrc;
 
-                        point = col0 * invYFrac + col1 * newYFrac;
+                        point = col0 * invYFrac + col1 * yFrc;
 
                         if (point > 255) { point = 255; } else if (point < 0) { point = 0; }
 
@@ -147,12 +149,10 @@ namespace ImageProcessing.App.DomainLayer.DomainModel.Scaling.Implementation
                 var srcStartPtr = (byte*)srcData.Scan0.ToPointer();
                 var dstStartPtr = (byte*)dstData.Scan0.ToPointer();
 
-                // guarantees that on coordinate transform (x, y) -> (ax, ay)
-                // the pointer of the source image will not reach
-                // the rightmost column and the lowest row
-                var srcWidth = src.Width - 1;
-                var srcHeight = src.Height - 1;
+                var (srcWidth, srcHeight) = (src.Width, src.Height);
+                var (xBound, yBound) = (srcWidth - 2, srcHeight - 2);
 
+                //(x, y) -> (ax, ay)
                 var yCoef = srcHeight / (double)dstHeight;
                 var xCoef = srcWidth / (double)dstWidth;
 
@@ -161,36 +161,40 @@ namespace ImageProcessing.App.DomainLayer.DomainModel.Scaling.Implementation
                 Parallel.For(0, dstHeight, options, y =>
                 {
                     var newY = y * yCoef;
-                    var newYFloor = (int)newY;
-                    var newYFrac = newY - newYFloor;
+                    var yFlr = (int)newY;
+                    var yFrc = newY - yFlr;
+
+                    if(yFlr > yBound) { yFlr = yBound; }
 
                     //get the address of a row
                     var dstRow = dstStartPtr + y * dstData.Stride;
 
-                    var i0 = srcStartPtr + newYFloor       * srcData.Stride;
-                    var i1 = srcStartPtr + (newYFloor + 1) * srcData.Stride;
+                    var i0 = srcStartPtr +  yFlr      * srcData.Stride;
+                    var i1 = srcStartPtr + (yFlr + 1) * srcData.Stride;
 
                     double col0, col1, point;
 
                     for (var x = 0; x < dstWidth; ++x, ++dstRow)
                     {
                         var newX = x * xCoef;
-                        var newXFloor = (int)newX;
-                        var newXFrac = newX - newXFloor;
+                        var xFlr = (int)newX;
+                        var xFrc = newX - xFlr;
 
-                        var j0 = newXFloor       * ptrStep;
-                        var j1 = (newXFloor + 1) * ptrStep;
+                        if (xFlr > xBound) { xFlr = xBound; }
+
+                        var j0 =  xFlr      * ptrStep;
+                        var j1 = (xFlr + 1) * ptrStep;
 
                         var p00 = i0 + j0; var p10 = i1 + j0;
                         var p01 = i0 + j1; var p11 = i1 + j1;
 
-                        var invXFrac = 1 - newXFrac;
-                        var invYFrac = 1 - newYFrac;
+                        var invXFrac = 1 - xFrc;
+                        var invYFrac = 1 - yFrc;
 
-                        col0 = p00[0] * invXFrac + p10[0] * newXFrac;
-                        col1 = p01[0] * invXFrac + p11[0] * newXFrac;
+                        col0 = p00[0] * invXFrac + p10[0] * xFrc;
+                        col1 = p01[0] * invXFrac + p11[0] * xFrc;
 
-                        point = col0 * invYFrac + col1 * newYFrac;
+                        point = col0 * invYFrac + col1 * yFrc;
 
                         if (point > 255) { point = 255; } else if (point < 0) { point = 0; }
 
