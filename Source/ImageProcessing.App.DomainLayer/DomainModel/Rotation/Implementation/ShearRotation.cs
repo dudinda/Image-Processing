83e77e85b3cedc584359fd4be9f95428e43cc0fd
Daniil Dudin <dudin0da@gmail.com>
2020-11-21 01:52:31 +0300

    implemented rotation by shear

---
+++ Source/ImageProcessing.App.DomainLayer/DomainModel/Rotation/Implementation/ShearRotation.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Drawing;
+
+using ImageProcessing.App.DomainLayer.DomainModel.Rotation.Interface;
+using ImageProcessing.App.DomainLayer.DomainModel.Transformation.Implementation;
+
+namespace ImageProcessing.App.DomainLayer.DomainModel.Rotation.Implementation
+{
+    public sealed class ShearRotation : IRotation
+    {
+        public Bitmap Rotate(Bitmap bmp, double rad)
+        {
+            var shear = new ShearTransformation();
+
+            // A(alpha)A(beta)A(gamma) = S(phi)
+            // where A is a shear matrix and S is a rotation matrix
+            // alpha = gamma = -tan(phi/2), beta = sin(phi)
+            var first  = shear.Transform(bmp, -Math.Tan(rad / 2), 0);
+            var second = shear.Transform(first, 0, Math.Sin(rad));
+
+            if (rad < 0.05) { return second; }
+
+            return shear.Transform(second, -Math.Tan(rad / 2), 0);
+        }
+            
+        
+    }
+}
