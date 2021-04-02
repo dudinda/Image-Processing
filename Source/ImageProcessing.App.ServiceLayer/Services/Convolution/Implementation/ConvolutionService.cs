ba8ce2be8b4c6167b53f296928f56f32110af512
Daniil Dudin <dudin0da@gmail.com>
2021-04-02 10:57:35 +0300

    updated convolution service

--- Source/ImageProcessing.App.ServiceLayer/Services/Convolution/Implementation/ConvolutionService.cs
+++ Source/ImageProcessing.App.ServiceLayer/Services/Convolution/Implementation/ConvolutionService.cs
@@ -19,11 +19,11 @@ namespace ImageProcessing.App.ServiceLayer.Services.Convolution.Implementation
 
             var destination = new Bitmap(source);
 
-            var sourceBitmapData = source.LockBits(
+            var srcData = source.LockBits(
                 new Rectangle(0, 0, source.Width, source.Height),
                 ImageLockMode.ReadOnly, source.PixelFormat);
 
-            var destinationBitmapData = destination.LockBits(
+            var dstData = destination.LockBits(
                 new Rectangle(0, 0, source.Width, source.Height),
                 ImageLockMode.WriteOnly, destination.PixelFormat);
 
@@ -32,9 +32,9 @@ namespace ImageProcessing.App.ServiceLayer.Services.Convolution.Implementation
 
             unsafe
             {
-                var sourceStartPtr      = (byte*)sourceBitmapData.Scan0.ToPointer();
-                var destinationStartPtr = (byte*)destinationBitmapData.Scan0.ToPointer();
-                var (srcStride, dstStride) = (sourceBitmapData.Stride, destinationBitmapData.Stride);
+                var srcStartPtr = (byte*)srcData.Scan0.ToPointer();
+                var dstStartPtr = (byte*)dstData.Scan0.ToPointer();
+                var (srcStride, dstStride) = (srcData.Stride, dstData.Stride);
 
                 var kernelOffset = (byte)((filter.Kernel.ColumnCount - 1) / 2);
 
@@ -46,33 +46,34 @@ namespace ImageProcessing.App.ServiceLayer.Services.Convolution.Implementation
                 Parallel.For(kernelOffset, height - kernelOffset, options, y =>
                 {
                     //get the address of a new line, considering a kernel offset
-                    var sourcePtr      = sourceStartPtr      + y * srcStride + kernelOffset * step;
-                    var destinationPtr = destinationStartPtr + y * dstStride + kernelOffset * step;
+                    var srcPtr = srcStartPtr + y * srcStride + kernelOffset * step;
+                    var dstPtr = dstStartPtr + y * dstStride + kernelOffset * step;
 
                     //accumulators of R, G, B components 
                     double r, g, b;
 
                     //addresses of elements in the radius of a convolution kernel
                     byte* rowPtr, colPtr;
+                    int rowIdx, colIdx;
 
-                    for (int x = kernelOffset; x < width - kernelOffset; ++x, sourcePtr += step, destinationPtr += step)
+                    for (int x = kernelOffset; x < width - kernelOffset; ++x, srcPtr += step, dstPtr += step)
                     {
-                        //set accumulators to 0
                         r = 0d; g = 0d; b = 0d;
 
                         for (var kernelRow = -kernelOffset; kernelRow <= kernelOffset; ++kernelRow)
                         {
-                            rowPtr = sourcePtr + kernelRow * srcStride;
+                            rowPtr = srcPtr + kernelRow * srcStride;
+                            rowIdx = kernelRow + kernelOffset;
 
                             for (var kernelColumn = -kernelOffset; kernelColumn <= kernelOffset; ++kernelColumn)
                             {
                                 //get the address of a current element
                                 colPtr = rowPtr + kernelColumn * step;
-
-                                //sum
-                                b += colPtr[0] * filter.Kernel[kernelRow + kernelOffset, kernelColumn + kernelOffset];
-                                g += colPtr[1] * filter.Kernel[kernelRow + kernelOffset, kernelColumn + kernelOffset];
-                                r += colPtr[2] * filter.Kernel[kernelRow + kernelOffset, kernelColumn + kernelOffset];
+                                colIdx = kernelColumn + kernelOffset;
+                                
+                                b += colPtr[0] * filter.Kernel[rowIdx, colIdx];
+                                g += colPtr[1] * filter.Kernel[rowIdx, colIdx];
+                                r += colPtr[2] * filter.Kernel[rowIdx, colIdx];
                             }
                         }
                         //multiply each component by the kernel factor
@@ -84,15 +85,13 @@ namespace ImageProcessing.App.ServiceLayer.Services.Convolution.Implementation
                         if (g > 255d) { g = 255d; } else if (g < 0d) { g = 0d; }
                         if (r > 255d) { r = 255d; } else if (r < 0d) { r = 0d; }
 
-                        destinationPtr[0] = (byte)b;
-                        destinationPtr[1] = (byte)g;
-                        destinationPtr[2] = (byte)r;
+                        dstPtr[0] = (byte)b; dstPtr[1] = (byte)g; dstPtr[2] = (byte)r;
                     }                   
                 });          
             }
 
-            source.UnlockBits(sourceBitmapData);
-            destination.UnlockBits(destinationBitmapData);
+            source.UnlockBits(srcData);
+            destination.UnlockBits(dstData);
 
             return destination;
         }
