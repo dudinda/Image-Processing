c42aefbd9f2ab1b0ed4251e3b2c77e075d8d9778
Daniil Dudin <dudin0da@gmail.com>
2021-07-07 18:32:48 +0300

    removed base class for the async locker

--- Source/ImageProcessing.App.ServiceLayer/Services/Locker/Implementation/AsyncOperationLocker.cs
+++ Source/ImageProcessing.App.ServiceLayer/Services/Locker/Implementation/AsyncOperationLocker.cs
@@ -2,17 +2,17 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-using ImageProcessing.App.ServiceLayer.Services.Locker.Base.Interface;
+using ImageProcessing.App.ServiceLayer.Services.LockerService.Operation.Interface;
 
-namespace ImageProcessing.App.ServiceLayer.Services.Locker.Base.Abstract
+namespace ImageProcessing.App.ServiceLayer.Services.LockerService.Operation.Implementation
 {
-    /// <inheritdoc cref="IAsyncLocker" />
-    public abstract class AsyncLockerBase : IAsyncLocker
+    /// <inheritdoc cref="IAsyncOperationLocker"/>
+    public class AsyncOperationLocker : IAsyncOperationLocker
     {
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
         /// <inheritdoc />
-        public async Task<TResult> LockAsync<TResult>(Func<TResult> worker)
+        public async Task<TResult> LockOperationAsync<TResult>(Func<TResult> worker)
         {
             await _semaphore.WaitAsync().ConfigureAwait(false);
             try
@@ -30,7 +30,7 @@ namespace ImageProcessing.App.ServiceLayer.Services.Locker.Base.Abstract
         }
 
         /// <inheritdoc />
-        public async Task LockAsync(Action worker)
+        public async Task LockOperationAsync(Action worker)
         {
             await _semaphore.WaitAsync().ConfigureAwait(false);
             try
--- Source/ImageProcessing.App.ServiceLayer/Services/Locker/Interface/IAsyncOperationLocker.cs
+++ Source/ImageProcessing.App.ServiceLayer/Services/Locker/Interface/IAsyncOperationLocker.cs
@@ -1,25 +1,24 @@
 using System;
 using System.Threading.Tasks;
 
-namespace ImageProcessing.App.ServiceLayer.Services.Locker.Base.Interface
+namespace ImageProcessing.App.ServiceLayer.Services.LockerService.Operation.Interface
 {
     /// <summary>
     /// Lock async the specified operation.
     /// Mainly used to perform a non - blocking deep copy 
     /// of the selected bitmap or to retrieve the actual copy
     /// during the application flow.
-    /// </summary>
-    internal interface IAsyncLocker
+    public interface IAsyncOperationLocker 
     {
         /// <summary>
         /// Lock async during the specified work.
         /// Returns the <typeparamref name="TResult"/>.
         /// </summary>
-        Task<TResult> LockAsync<TResult>(Func<TResult> worker);
+        Task<TResult> LockOperationAsync<TResult>(Func<TResult> worker);
 
         /// <summary>
         /// Lock async during the specified action.
         /// </summary>
-        Task LockAsync(Action worker);
+        Task LockOperationAsync(Action worker);
     }
 }
